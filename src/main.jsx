556502c07aaf6c7eb6ef0b1b7f75e3482164b5cb
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2023-02-02 13:48:54 +0300

    feat: delete files and add logo image

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Normalize } from 'styled-normalize'
-import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Normalize />
-    <App />
   </React.StrictMode>,
 )
